a9035da923d990ac9c84b20220e709a525e8d1c2
BlainedZA <blainedza@gmail.com>
2021-02-26 17:44:02 +0200

    Add Reactjs

---
+++ src/js/components/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import "src/css/App.css";
+
+//Import Components
+import Nav from './Nav'
+
+
+const App = () => (
+    <div>
+        <Nav />
+        <h1>test</h1>
+    </div>
+);
+
+export default App;
---
+++ src/js/components/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "src/css/Nav.css";
+
+const Nav = () => (
+    <div className="container">
+        <nav>
+            <h1>test</h1>
+        </nav>
+    </div>
+);
+
+export default Nav;
